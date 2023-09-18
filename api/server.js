8df6f8a8437b41bf93ab761e4e8feff73c447c4c
ghalioussama <110343529+ghalioussama@users.noreply.github.com>
2023-09-18 20:42:57 +0100

    Update server.js

--- api/server.js
+++ api/server.js
@@ -4,14 +4,7 @@ const http = require('http');
 const url = require('url');
 
 const server = http.createServer((req, res) => {
-  fs.writeFile('board.json', 'محتوى الملف', (err) => {
-  if (err) throw err;
-  console.log("create")
-});
-fs.appendFile('board.json', 'النص الإضافي', (err) => {
-  if (err) throw err;
-  console.log('تمت إضافة النص بنجاح');
-});
+  
   
 });
 
