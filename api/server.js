933a20b7ef03b2dd2ec307e67a566d8074d1fd8b
ghalioussama <110343529+ghalioussama@users.noreply.github.com>
2023-09-18 20:36:05 +0100

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,22 +1,53 @@
-const fs = require('fs');
-const http = require('http');
-const url = require('url');
-const jsonServer = require('json-server');
 const jsonServer = require('json-server');
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
-const server = http.createServer((req, res) => {fs.writeFile('board.json', 'محتوى الملف', (err) => {
-  if (err) throw err;
-  console.log("create")
-});
-fs.appendFile('board.json', 'النص الإضافي', (err) => {
-  if (err) throw err;
-  console.log('تمت إضافة النص بنجاح');
-});
+const server = http.createServer((req, res) => {
+  const query = url.parse(req.url, true).query; // استخراج المتغيرات من عنوان الطلب
 
-  
+  // التحقق من وجود متغير "points" في متغيرات GET
+  if (query.points !== undefined) {
+
+    const pointsToAdd = parseInt(query.points, 10); // تحويل قيمة points إلى عدد صحيح
+
+    // قراءة الملف JSON
+       const filePath = '../db.json';
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        console.error('خطأ في قراءة الملف:', err);
+        return;
+      }
+
+      const jsonData = JSON.parse(data);
+
+      // زيادة النقاط بناءً على القيمة الممررة في متغير "points"
+      for (let i = 0; i < jsonData.length; i++) {
+        if (jsonData[i].code === "MA") {
+          // زيادة النقاط هنا
+          jsonData[i].points += pointsToAdd;
+        }
+      }
+
+      // حفظ التغييرات في الملف
+      const updatedData = JSON.stringify(jsonData, null, 2); // null و 2 تجعل التنسيق أكثر قراءة
+      fs.writeFile(filePath, updatedData, 'utf8', (err) => {
+        if (err) {
+          console.error('خطأ في حفظ الملف:', err);
+          return;
+        }
+        console.log('تم زيادة النقاط بنجاح.');
+      });
+    });
+
+    // إجراء رد الاستجابة إلى المستخدم
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('تم زيادة النقاط بنجاح.');
+  } else {
+    // إذا لم يتم تمرير متغير "points" في الطلب
+    res.writeHead(400, { 'Content-Type': 'text/plain' });
+    res.end('الرجاء تمرير متغير "points" في الطلب.');
+  }
 });
 
 server.listen(3000, () => {
