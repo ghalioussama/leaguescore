9a529f4bf68b804a59ebee6f1e83ca8c4f9d5e72
ghalioussama <110343529+ghalioussama@users.noreply.github.com>
2023-09-18 20:55:11 +0100

    Update server.js

--- api/server.js
+++ api/server.js
@@ -4,14 +4,6 @@ const http = require('http');
 const url = require('url');
 
 const server = http.createServer((req, res) => {
-fs.writeFile('board.json', 'محتوى الملف', (err) => {
-  if (err) throw err;
-  console.log("create")
-});
-fs.appendFile('board.json', 'النص الإضافي', (err) => {
-  if (err) throw err;
-  console.log('تمت إضافة النص بنجاح');
-});
   const query = url.parse(req.url, true).query; // استخراج المتغيرات من عنوان الطلب
 
   // التحقق من وجود متغير "points" في متغيرات GET
@@ -55,7 +47,7 @@ fs.appendFile('board.json', 'النص الإضافي', (err) => {
     // إذا لم يتم تمرير متغير "points" في الطلب
     res.writeHead(400, { 'Content-Type': 'text/plain' });
     res.end('الرجاء تمرير متغير "points" في الطلب.');
-           }
+  }
 });
 
 server.listen(3000, () => {
